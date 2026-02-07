1775176b5a1c6096d1dd95a6e599cad7a6bf3a73
bmeben01 <brian.ebenschweiger@gmail.com>
2026-02-06 22:16:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,17 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Brianna",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine? ❤️❤️❤️",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        bears: ['🧸', '🐻', '🐶', '🫶']                       // Cute bear emojis
     },
 
     // Questions and answers
